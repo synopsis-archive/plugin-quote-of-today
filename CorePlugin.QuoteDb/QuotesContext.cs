61dc738ef1873a84ba374c178c8777282df13ad0
sgrei <sgreinecker32@yahoo.com>
2022-12-19 09:42:52 +0100

    Big Big Prablem

--- CorePlugin.QuoteDb/QuotesContext.cs
+++ CorePlugin.QuoteDb/QuotesContext.cs
@@ -32,18 +32,10 @@ public class QuotesContext : DbContext
                     SubmitterUuid = new Guid("00000000-0000-0000-0000-000000000000"),
                     SubmittedBy = "Loata",
                     SubmitTime = DateTime.Now
-                },
+                }
                 new Quote
                 {
                     QuoteId = 3,
-                    QuoteText = "Âcht",
-                    SubmitterUuid = new Guid("00000000-0000-0000-0000-000000000000"),
-                    SubmittedBy = "Tops",
-                    SubmitTime = DateTime.Now
-                },
-                new Quote
-                {
-                    QuoteId = 4,
                     QuoteText = "Do you need a We-e-ee?",
                     SubmitterUuid = new Guid("00000000-0000-0000-0000-000000000000"),
                     SubmittedBy = "Loata",
@@ -51,7 +43,7 @@ public class QuotesContext : DbContext
                 },
                 new Quote
                 {
-                    QuoteId = 5,
+                    QuoteId = 4,
                     QuoteText = "I gib da glei so a Backpfeife!",
                     SubmitterUuid = new Guid("00000000-0000-0000-0000-000000000000"),
                     SubmittedBy = "Andi",
