5936d29d1b14d2f0add881ef6c088cc19f6f31f9
Klaus Scheiböck <klaus@vortexdata.net>
2023-04-20 21:36:34 +0200

    test

--- CorePlugin.Plugin/ConfigurationExtensions.cs
+++ CorePlugin.Plugin/ConfigurationExtensions.cs
@@ -6,6 +6,7 @@ public static partial class ConfigurationExtensions
 {
     public static string? GetConnectionStringThatAlsoWorksInProduction(this IConfiguration configuration, string name, bool isDevelopment = false)
     {
+        Console.WriteLine(configuration.GetConnectionString(isDevelopment ? name : "core")?.Replace("core", name.ToLower()) ?? null);
         return configuration.GetConnectionString(isDevelopment ? name : "core")?.Replace("core", name.ToLower()) ?? null;
     }
 }
