1759589ec2688b3910f2232ed03058267d778682
Klaus Scheiböck <klaus@vortexdata.net>
2023-04-20 21:55:04 +0200

    Update ConfigurationExtensions.cs

--- CorePlugin.Plugin/ConfigurationExtensions.cs
+++ CorePlugin.Plugin/ConfigurationExtensions.cs
@@ -6,7 +6,7 @@ public static partial class ConfigurationExtensions
 {
     public static string? GetConnectionStringThatAlsoWorksInProduction(this IConfiguration configuration, string name, bool isDevelopment = false)
     {
-        Console.WriteLine(configuration.GetConnectionString(isDevelopment ? name : "core")?.Replace("core", name.ToLower()) ?? "oida na bitte");
+        Console.WriteLine(configuration.GetConnectionString(isDevelopment ? name : "Production")?.Replace("core", name.ToLower()) ?? "oida na bitte");
         return configuration.GetConnectionString(isDevelopment ? name : "Production")?.Replace("core", name.ToLower()) ?? null;
     }
 }
